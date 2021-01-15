aa1c42e43147e1a6efd6cbe308d1b26b28afb72e
anastasiacalej <anastasiacalej@gmail.com>
2021-01-15 13:52:06 +0200

    done

---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,31 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+    colors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  };
+
+  render() {
+   
+    return (
+      <div style={{width:"40%",margin:"0 auto",fontWeight:"bold", fontSize:"30px", textAlign:"center",lineHeight:"250%"}}>
+          {
+            this.props.colors.reduce((r,v)=>
+            {
+               return (
+                 <div style={{border:"solid 10px "+v,padding:"10px"}}>
+                   {r}
+                 </div>
+               );
+            },
+              this.props.children
+            )
+          }
+      </div>
+    ); 
+  }
+}
+
+export default RainbowFrame;
---
+++ br2jsx/components/Br2jsx.js
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class Br2jsx extends React.Component {
+
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+  };
+
+  render() {
+ 
+    return (
+      
+      <div style={{width:"150px",padding:"20px", backgroundColor:"#13594c", fontWeight:"bold", 
+           fontSize:"24px", color:"white", textAlign:"center"}}>
+          {
+           this.props.text.split(/<br\s?\/?>/g).reduce((r,v,i,a)=>{
+              r.push(v);
+              (i<a.length-1)&&r.push(<br key={i}/>);
+           return r;
+           },[])
+          }
+      </div>
+    
+    ); 
+  }
+}
+
+export default Br2jsx;
