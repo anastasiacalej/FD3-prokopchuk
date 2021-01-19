7f8a6644b58d41547a47c5d09d05c3e39b8ddfb3
anastasiacalej <anastasiacalej@gmail.com>
2021-01-19 20:37:36 +0200

    corrected

--- RainbowFrameHOC/components/withRainbowFrame.js
+++ RainbowFrameHOC/components/withRainbowFrame.js
@@ -1,34 +1,32 @@
 ﻿import React from 'react';
 
-    let withRainbowFrame = (colors,code)=>Component=>props=> 
-      (<div style={{width:"600px",textAlign:"center"}}>
-
-          { 
-            //первый вариант через forEach:
-
-            code=<Component {...props}/>,           
-            colors.forEach(color =>           
-              code=<div style={{border:"solid 5px "+color,padding:"10px"}}>{code}</div>
-            ),
-            code
-
+    let withRainbowFrame = (colors)=>Component=>props=>
+      //первый вариант через forEach: 
+      //  {
+      //     let code=<Component {...props}/>;           
+      //     colors.forEach(color =>           
+      //       code=<div style={{border:"solid 5px "+color,padding:"10px"}}>{code}</div>
+      //     );
+      //     return (
+      //       <div style={{width:"600px",textAlign:"center"}}>
+      //         {code}
+      //       </div>
+      //     )
+      //  }
+      
             //-----------------------------
             //второй вариант через reduce:
-
-            // colors.reduce((r,v)=>
-            // {
-            //    return (
-            //      <div style={{border:"solid 10px "+v,padding:"10px"}}>
-            //        {r}
-            //      </div>
-            //    );
-            // },
-            //   <Component {...props}/>
-            // )
-
-          }
-      </div>
-      ); 
-
+        <div style={{width:"600px",textAlign:"center"}}>
+           { colors.reduce((r,color)=>
+            {
+               return (
+                 <div style={{border:"solid 10px "+color,padding:"10px"}}>
+                   {r}
+                 </div>
+               );
+            },
+              <Component {...props}/>
+            )}
+        </div>
 
 export {withRainbowFrame};
