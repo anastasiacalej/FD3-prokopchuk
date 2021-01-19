3b0af64d7b2201bc4f98d81f1fa3e60a06fb0019
anastasiacalej <anastasiacalej@gmail.com>
2021-01-19 13:24:51 +0200

    done

---
+++ RainbowFrameHOC/components/DoubleButton.js
@@ -0,0 +1,33 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class DoubleButton extends React.Component {
+
+  static propTypes = {
+    caption1: PropTypes.string.isRequired,
+    caption2: PropTypes.string.isRequired,
+    cbPressed: PropTypes.func.isRequired
+  };
+
+  pressed1=()=>{
+    this.props.cbPressed(1);
+  }
+
+  pressed2=()=>{
+    this.props.cbPressed(2);
+  }
+
+  render() {
+   
+    return (
+      <div>
+          <input type='button' value={this.props.caption1} onClick={this.pressed1} style={{marginRight:'10px'}}/>
+          {this.props.children}
+          <input type='button' value={this.props.caption2} onClick={this.pressed2} style={{marginLeft:'10px'}}/>
+      </div>
+      
+    ); 
+  }
+}
+
+export default DoubleButton;
---
+++ RainbowFrameHOC/components/withRainbowFrame.js
@@ -0,0 +1,34 @@
+﻿import React from 'react';
+
+    let withRainbowFrame = (colors,code)=>Component=>props=> 
+      (<div style={{width:"600px",textAlign:"center"}}>
+
+          { 
+            //первый вариант через forEach:
+
+            code=<Component {...props}/>,           
+            colors.forEach(color =>           
+              code=<div style={{border:"solid 5px "+color,padding:"10px"}}>{code}</div>
+            ),
+            code
+
+            //-----------------------------
+            //второй вариант через reduce:
+
+            // colors.reduce((r,v)=>
+            // {
+            //    return (
+            //      <div style={{border:"solid 10px "+v,padding:"10px"}}>
+            //        {r}
+            //      </div>
+            //    );
+            // },
+            //   <Component {...props}/>
+            // )
+
+          }
+      </div>
+      ); 
+
+
+export {withRainbowFrame};
